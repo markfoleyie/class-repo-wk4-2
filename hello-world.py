952d78b6deeeac7a7f7dda08b20d904a4ad6e5c7
Mark Foley <mark@markfoley.info>
2021-02-25 19:11:50 +0000

    changed hello-world in wk5-test-1 branch

--- hello-world.py
+++ hello-world.py
@@ -1,5 +1,10 @@
 """
 a program
 """
-print("Hello World")
-print(f"Filename is {__file__}. Name is {__name__}.\n Docstring is {__doc__}.")
+
+def main():
+    print("Hello World")
+    print(f"Filename is {__file__}. Name is {__name__}.\n Docstring is {__doc__}.")
+
+if __name__ == "__main__":
+    main()
